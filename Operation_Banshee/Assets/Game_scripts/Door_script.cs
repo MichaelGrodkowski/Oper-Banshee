9daa36e283db980db4e65fa09036014e32c0299b
Michael Grodkowski <michael_grodkowski@yahoo.com>
2020-04-07 20:25:17 -0600

    Shader Info on baking lights.

---
+++ Operation_Banshee/Assets/Game_scripts/Door_script.cs
@@ -0,0 +1,49 @@
+﻿ using System;
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_script : MonoBehaviour
+{
+    [SerializeField] private GameObject DoorMessage;
+    private Boolean CanOpen = false;
+    private Animator Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DoorMessage.gameObject.SetActive(false);
+        Anim = this.transform.parent.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CanOpen == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Anim.SetTrigger("Open");
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            DoorMessage.gameObject.SetActive(true);
+            CanOpen = true; 
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            DoorMessage.gameObject.SetActive(false);
+            CanOpen = false; 
+            
+        }
+    }
+}
---
+++ Operation_Banshee/Assets/Game_scripts/JustOpen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JustOpen : MonoBehaviour
+
+{
+    private Animator _doorAnim;
+
+    void OnTriggerEnter(Collider other)
+    {
+        _doorAnim.SetBool("isOpened", true);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _doorAnim = this.transform.parent.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
