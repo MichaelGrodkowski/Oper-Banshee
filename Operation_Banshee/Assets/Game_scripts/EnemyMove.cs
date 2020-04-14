b8b4823e97218847041699e33db6e1638bf08b69
Michael Grodkowski <michael_grodkowski@yahoo.com>
2020-04-14 16:11:30 -0600

    reimport of animations and enemy

---
+++ Operation_Banshee/Assets/Game_scripts/EnemyMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour
+{
+
+    [SerializeField] private Transform Target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveToTarget();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MoveToTarget()
+    {
+        GetComponent<NavMeshAgent>().destination = Target.position;
+    }
+}
