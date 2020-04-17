6b8a1af4d37a366a7fb247af81a98aab3acf3d93
Michael Grodkowski <michael_grodkowski@yahoo.com>
2020-04-17 13:17:01 -0600

    Working on Projectiles

---
+++ Operation_Banshee/Assets/Game_scripts/Shooting/Laser_Script.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser_Script : MonoBehaviour
+{
+
+    public GameObject firePoint;
+    public List<GameObject> vfx = new List<GameObject>();
+
+    private GameObject effectToSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        effectToSpawn = vfx[0];
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            SpawnVFX();
+        }
+    }
+
+    void SpawnVFX()
+    {
+        GameObject vfx;
+        if (firePoint != null)
+        {
+            vfx = Instantiate(effectToSpawn, firePoint.transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Debug.Log("No Fire Point");
+        }
+    }
+}
---
+++ Operation_Banshee/Assets/Game_scripts/Shooting/RigidShoot.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidShoot : MonoBehaviour
+{
+    public GameObject LaserPreFab;
+    public GameObject muzzleFlash;
+    public Transform firePoint;
+
+    public float shotpower = 200f;
+    public static bool CanShoot = false;
+
+    void Start()
+    {
+        if (firePoint == null)
+            firePoint = transform;
+        
+        InvokeRepeating("Shooting", 0.1f, 0.5f);
+    }
+    
+    void Shoot()
+    {
+        GameObject Laser;
+        Laser = Instantiate(LaserPreFab, firePoint.position, firePoint.rotation);
+        Laser.GetComponent<Rigidbody>().AddForce(firePoint.forward * shotpower);
+
+        Save_Script.Lasers -= 1;
+        if (Save_Script.Lasers <= 0) ;
+        {
+            Save_Script.Lasers = 0;
+        }
+        
+        GameObject tempFlash;
+        Instantiate(LaserPreFab, firePoint.position, firePoint.rotation).GetComponent<Rigidbody>().AddForce(firePoint.forward * shotpower);
+        tempFlash = Instantiate(muzzleFlash, firePoint.position, firePoint.rotation);
+
+        Destroy(Laser, 2.0f);
+        Destroy(muzzleFlash, 0.03f);
+    }
+
+    void Shooting()
+    {
+        if (CanShoot == true)
+        {
+            if (Save_Script.Lasers > 0)
+            {
+                GetComponent<Animator>().SetTrigger("Fire");
+                if (Save_Script.HaveBeenShot == true)
+                {
+                    //HurtUI
+                }
+            }
+
+        }
+    }
+}
